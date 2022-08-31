7cfca9a44b736b5d7d65cbc77a135cc337f5ce01
YOOJUNGIL <xcon9999@gmail.com>
2022-08-31 22:52:42 +0900

    18.useCallback 함수재사용

--- src/components/ContentsCRUD.js
+++ src/components/ContentsCRUD.js
@@ -1,5 +1,8 @@
 import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 
+// useMemo      : 특정 결과값을 재사용 할 때 사용
+// useCallback  : 특정 함수를 재사용 할 때 사용, 배열에 값들이 최신값으로핸들링됨
+
 // 활성사용자수
 function countActiveUsers(users) {
     const activeCount = users.filter(user => user.active).length;
@@ -68,17 +71,17 @@ function ContentsCRUD(props) {
     const {userName, email} = inputs;
 
     //function onChange(e){} es5에서 es6 대체문법 적용
-    const onChange = (e) => {
+    const onChange = useCallback((e) => {
         const {name, value} = e.target;
         setInputs({
             ...inputs,
             [name]: value
         });
-    };
+    }, [inputs]);
 
     // 추가
     //function onCreate(){} es5에서 es6 대체문법 적용
-    const onCreate = () => {
+    const onCreate = useCallback(() => {
         const user = {
             id: nextId.current,
             userName: userName,     //만약에 key 이름으로 선언된 값이 존재하다면, 바로 매칭시켜줄수 있으므로 생략 가능(es6 문법), onChange에의해값이 이미 세팅되어져 있음
@@ -96,19 +99,19 @@ function ContentsCRUD(props) {
 
         // id 등록될때마다 1증가
         nextId.current += 1;
-    };
+    }, [users, userName, email]);
 
     // 삭제
     //function onRemove(id){} es5에서 es6 대체문법 적용
-    const onRemove = (id) => {
+    const onRemove = useCallback((id) => {
         setUsers(users.filter(user => user.id !== id));
-    };
+    }, [users]);
 
     // 반전
     //function onToggle(id){} es5에서 es6 대체문법 적용
-    const onToggle = (id) => {
+    const onToggle = useCallback((id) => {
         setUsers(users.map(user => user.id === id ? {...user, active: !user.active} : user));
-    };
+    }, [users]);
 
     // id초기값
     const nextId = useRef(4);
